796839fbe011a9b68b04d20d4fa3fabe4c5f0612
lmignone1 <118554285+lmignone1@users.noreply.github.com>
2023-12-02 12:58:24 +0100

    first push

---
+++ detector_v5.py
@@ -0,0 +1,102 @@
+import torch
+import cv2
+import numpy as np
+import os
+import os
+
+FILE_NAME =  os.path.dirname(os.path.realpath(__file__)) + '/IMG_3061.MOV'
+
+class YoloDetector():
+
+    def __init__(self):
+        # Using yolov5s for our purposes of object detection, you may use a larger model
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        # model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'yolov7-tiny.torchscript.pt')
+        # self.model = torch.jit.load(model_path)
+        self.classes = self.model.names
+        print(self.classes)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print('Using Device:', self.device)
+    
+    def score_frame(self, frame):
+        self.model.to(self.device)
+        downscale_factor = 2
+        width = int(frame.shape[1] / downscale_factor)
+        height = int(frame.shape[0] / downscale_factor)
+        frame = cv2.resize(frame, (width, height))
+
+        results = self.model(frame)
+
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+
+        return labels, cord
+    
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+    
+    def plot_boxes(self, results, frame, height, width, confidence=0.3):
+
+        labels, cord = results
+        detections = []
+
+        n = len(labels)
+        x_shape, y_shape = width, height
+
+        for i in range(n):
+            row = cord[i]
+
+            if row[4]>=confidence:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                
+                #In this demonstration, we will only be detecting persons. You can add classes of your choice
+                if self.class_to_label(labels[i]) == 'person':
+
+                    x_center = x1 + (x2-x1)
+                    y_center = y1 + ((y2-y1) / 2)
+
+                    tlwh = np.asarray([x1, y1, int(x2-x1), int(y2-y1)], dtype = np.float32)
+                    confidence = float(row[4].item())
+                    feature = 'car'
+
+                    detections.append(([x1, y1, int(x2), int(y2)], row[4].item(), 'person'))
+        
+        return frame, detections
+    
+
+if __name__ == '__main__':
+    detector = YoloDetector()
+    # cap = cv2.VideoCapture(0)
+    # while True:
+    #     _, frame = cap.read()
+    #     frame = cv2.flip(frame, 1)
+    #     height, width = frame.shape[:2]
+    #     labels, cord = detector.score_frame(frame)
+    #     results = (labels, cord)
+    #     frame, detections = detector.plot_boxes(results, frame, height, width)
+    #     cv2.imshow('frame', frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+    video = cv2.VideoCapture(FILE_NAME)
+
+    # Setting resolution for webcam
+
+    #Initializing the detection class
+    detector = YoloDetector()
+
+    while True:
+        _, frame = video.read()
+        #frame = cv2.flip(frame, 1)
+        height, width = frame.shape[:2]
+        results = detector.score_frame(frame)
+        frame, detections = detector.plot_boxes(results, frame, height, width)
+        for i in detections:
+            i = i[0]
+            cv2.rectangle(frame, (int(i[0]),int(i[1])),(int(i[2]),int(i[3])),(0,0,255),2)
+        cv2.imshow('frame', cv2.resize(frame, (int(width/2), int(height/2))))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
